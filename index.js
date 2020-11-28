b9475b532f1f6b1a0d65a7d1e626fcb3038ae338
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-28 05:32:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,23 +7,18 @@ function bfs(rootNode, vertices, edges){
   rootNode.distance = 0
   
   queue.push(rootNode)
-//  console.log(queue)
+
   while(queue !== null)
   {
-    //console.log("here")
+
     let firstNode = queue.shift()
-    //console.log(firstNode)
     let adjacentVertices = findAdjacent(firstNode.name, vertices, edges)
-    //console.log("here")
     
     markDistanceAndPredecessor(firstNode, adjacentVertices)
-    
-//    console.log(adjacentVertices)
     addTo(adjacentVertices, queue)  
     visited.push(firstNode)
-//    break
   }
- // console.log(visited)
+
   return visited
 
 }
@@ -35,7 +30,7 @@ function findAdjacent(node, vertices, edges)
   var foundNode
   var adjObj
   
-  //console.log (`\n\n\n\n NEW TEST`, vertices)
+
       
   for (var i = 0; i < edges.length; i++)
   {
@@ -57,35 +52,34 @@ function findAdjacent(node, vertices, edges)
       }
     }
   }
-  //console.log(adjArray)
+
   return adjArray
   
 }
 
 function getObj(vertices, name)
 {
-  //console.log (vertices, name)
-  //console.log (vertices.find(element => (element.name === name)));
+
   return vertices.find(element => (element.name === name));
   
 }
 
 function markDistanceAndPredecessor(vertex, vertices)
 {
-  //console.log(vertices)
+
   for( var i = 0; i < vertices.length; i++)
   {
     vertices[i].distance = vertex.distance + 1;
     vertices[i].predecessor = vertex;
   }
-  //console.log(vertices)
+
   return vertices;
   
 }
 
 function addTo(vertices, list)
 {
-//  console.log(vertices)
+
   for (var i = 0; i < vertices.length; i++)
   {
     list.push(vertices[i])
