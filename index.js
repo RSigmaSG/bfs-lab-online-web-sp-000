7c353e051e8f34ecc45ebe0eb7c4dba9c429503a
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-11-28 05:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,90 @@
 function bfs(rootNode, vertices, edges){
 
+  rootNode = vertices[0]
+  let queue = []
+  let visited = []
+  
+  rootNode.distance = 0
+  
+  queue.push(rootNode)
+  console.log(queue)
+  while(queue !== null)
+  {
+    //console.log("here")
+    let firstNode = queue.shift()
+    let adjacentVertices = findAdjacent(firstNode, vertices, edges)
+    //console.log("here")
+    
+    markDistanceAndPredecessor(firstNode, adjacentVertices)
+    addTo(adjacentVertices, queue)  
+    visited.push(firstNode)
+    
+  }
+  return visited
+
+}
+
+function findAdjacent(node, vertices, edges)
+{
+  
+  var adjArray = []
+  var foundNode
+  var adjObj
+  
+  //console.log (`\n\n\n\n NEW TEST`, vertices)
+      
+  for (var i = 0; i < edges.length; i++)
+  {
+
+    if (edges[i][0] === node )
+    {
+      adjObj = getObj(vertices, edges[i][1]) 
+      if(adjObj.distance === null)
+      {
+        adjArray.push(adjObj);
+      }
+    }
+    else if (edges[i][1] === node)
+    {
+      adjObj = getObj(vertices, edges[i][0]) 
+      if(adjObj.distance === null)
+      {
+        adjArray.push(adjObj);
+      }
+    }
+  }
+  
+  return adjArray
+  
+}
+
+function getObj(vertices, name)
+{
+  //console.log (vertices, name)
+  //console.log (vertices.find(element => (element.name === name)));
+  return vertices.find(element => (element.name === name));
+  
+}
+
+function markDistanceAndPredecessor(vertex, vertices)
+{
+  //console.log(vertices)
+  for( var i = 0; i < vertices.length; i++)
+  {
+    vertices[i].distance = vertex.distance + 1;
+    vertices[i].predecessor = vertex;
+  }
+  //console.log(vertices)
+  return vertices;
+  
+}
+
+function addTo(vertices, list)
+{
+  console.log(vertices)
+  for (var i = 0; i < vertices.length; i++)
+  {
+    list.push(vertices[i])
+  }
+  
 }
